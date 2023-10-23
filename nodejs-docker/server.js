c6bb2290df808e2e790b76800c75888a5874913a
jeonghunBang <wjdgns2751@naver.com>
2023-10-23 11:21:53 +0900

    docker lecture first commit

---
+++ nodejs-docker/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const PORT = 8080;
+
+//APP
+
+const app = express();
+app.get('/',(req,res)=>{
+    res.send("안녕하세요");
+});
+
+app.listen(PORT);
+console.log('sever is running ');
